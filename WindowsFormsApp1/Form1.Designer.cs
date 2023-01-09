2691ce04d714d97db240a5ebf9ac6de3b9015b10
dolezinski.jan <dolezinski.jan@STACJA105-15>
2023-01-09 13:26:49 +0100

    Dodaj pliki projektów.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBoxLista = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TextBoxLista
+            // 
+            this.TextBoxLista.Location = new System.Drawing.Point(12, 12);
+            this.TextBoxLista.Name = "TextBoxLista";
+            this.TextBoxLista.Size = new System.Drawing.Size(341, 338);
+            this.TextBoxLista.TabIndex = 0;
+            this.TextBoxLista.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(388, 327);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Sortuj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TextBoxLista);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox TextBoxLista;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using ListaUczestnikow.ModelDanych;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WinFormsApp1.ModelDanych;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Pobieracz pobieracz;
+        public Form1()
+        {
+            InitializeComponent();
+            pobieracz = new Pobieracz();
+            pobieracz.PobierzDaneZBazy();
+            PokazDane();
+
+        }
+        public void PokazDane()
+        {
+            TextBoxLista.Clear();
+            foreach (Osoba osoba in pobieracz.ListaOsob)
+                TextBoxLista.Text += osoba.ToString();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pobieracz.ListaOsob.Sort();
+            PokazDane();
+        }
+    }
+}
